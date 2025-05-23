6724add7ec189797df4760cbac4dba0fca00ffb5
h+ <serdar@justserdar.dev>
2025-05-23 14:23:58 +0200

    refactor(components): clean up import file extensions

---
+++ playground/app/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// for testing purposes with auto-import disabled
+import { TresCanvas } from '@tresjs/core'
+</script>
+
+<template>
+  <TresCanvas
+    window-size
+    render-mode="on-demand"
+  >
+    <TresPerspectiveCamera />
+    <AnimatedDonnut color="orange" />
+    <OrbitControls />
+  </TresCanvas>
+</template>
